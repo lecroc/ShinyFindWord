37d748819b98f6c9c5f7649866c389dcb88c1514
lecroc <clecrone@compasspoint-media.com>
2018-01-31 20:19:21 -0600

    work from 1-31-18 2

--- FindWord/ui.R
+++ FindWord/ui.R
@@ -35,6 +35,7 @@ shinyUI(fluidPage(
       h2("Instructions:"),
       h6("1. Enter phrase below"),
       h6("2. Click Submit to see suggested words"),
+      h6("3. Default is 5 most common words"),
       h2(" "),
       textInput("InputText","Input Text Here", value=""),
        submitButton("Submit")
@@ -43,7 +44,7 @@ shinyUI(fluidPage(
       h4("Your Input:"),
       textOutput("newtext"),
       h2(" "),
-      h4("Suggested Words:"),
+      h4("Suggested next Words:"),
       textOutput("words1"),
       textOutput("words2"),
       textOutput("words3"),
